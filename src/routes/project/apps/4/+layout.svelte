77a65e00bdfa5059a8b4040a245fccb610bef80a
Baka <vera.yasak@gmail.com>
2024-03-14 10:04:58 +0500

    add main page and nav

---
+++ src/routes/project/apps/4/+layout.svelte
@@ -0,0 +1,12 @@
+<div class='container'>
+    <a href='/project'>Назад</a>
+    <slot/>
+</div>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+</style>
